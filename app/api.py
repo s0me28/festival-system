d476bc2a6315ecc338f55c301e06ab5ceb242578
s0me28 <adriansomeseanu@gmail.com>
2023-11-01 16:15:36 +0200

    Application

--- app/api.py
+++ app/api.py
@@ -1,23 +1,39 @@
 from flask import Flask, render_template, request, jsonify
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
 import json
 import datetime
+import spacy
+from fuzzywuzzy import fuzz
 
-#REST API
+# REST API
 app = Flask(__name__)
+
+# spacy
+nlp = spacy.load("en_core_web_sm")
 
 # Pisoiul
 chatbot = ChatBot("Pisoiul")
+#i miss you bac
 
 # Load Bus Schedule data
 with open("bus_schedule.json", "r") as file:
     bus_schedules = json.load(file)
 
-# Get the current day
+# Current day
 days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 current_day_time = datetime.datetime.now().weekday()
 current_day = days_of_the_week[current_day_time]
+
+# fuzzy string matching
+def get_best_matching_query(user_text, possible_queries):
+    best_match = None
+    best_score = 0
+    for query in possible_queries:
+        score = fuzz.ratio(user_text.lower(), query.lower())
+        if score > best_score:
+            best_score = score
+            best_match = query
+    return best_match
 
 # Get bus schedule
 def get_bus_schedule(bus_line, day):
@@ -37,12 +53,18 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
 
-    if userText.startswith("What is the schedule of the bus "):
-        bus_line = userText.split("bus ", 1)[1]
-        response = get_bus_schedule(bus_line, current_day)
+    possible_queries = [
+        "What is the schedule of the bus"
+    ]
+
+    # identify the user's intention
+    best_matching_query = get_best_matching_query(userText, possible_queries)
+    if best_matching_query:
+        if best_matching_query.lower() == "what is the schedule of the bus":
+            bus_line = userText.split("bus ", 1)[1]
+            response = get_bus_schedule(bus_line, current_day)
     else:
         response = chatbot.get_response(userText).text
-
     return jsonify({'response': response})
 
 if __name__ == "__main__":
